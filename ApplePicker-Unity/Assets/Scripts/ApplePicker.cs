a6719e193f62acf4a62c59be82f660b7e0c7a11f
RoyZhang8 <royzhang991220@gmail.com>
2022-02-07 15:01:41 -0600

    UI update

--- ApplePicker-Unity/Assets/Scripts/ApplePicker.cs
+++ ApplePicker-Unity/Assets/Scripts/ApplePicker.cs
@@ -26,4 +26,12 @@ public class ApplePicker : MonoBehaviour
     {
         
     }
+    public void AppleDestroyed()
+    {
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach(GameObject tGo in tAppleArray)
+        {
+            Destroy(tGo);
+        }
+    }
 }
--- ApplePicker-Unity/Assets/Scripts/Basket.cs
+++ ApplePicker-Unity/Assets/Scripts/Basket.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject scoreGO = GameObject.Find("ScoreCounter"); //score game object
+        scoreGT = scoreGO.GetComponent<Text>(); //the next component of the score GO
+        scoreGT.text = "0"; // set the text property
     }
 
     // Update is called once per frame
@@ -33,6 +40,10 @@ public class Basket : MonoBehaviour
         if (collidedWith.tag == "Apple")
         { // 4
             Destroy(collidedWith);
+
+            int score = int.Parse(scoreGT.text);
+            score += 100;
+            scoreGT.text = score.ToString();
         }
     }
 }
