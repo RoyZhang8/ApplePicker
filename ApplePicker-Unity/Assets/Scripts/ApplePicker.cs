b84e9d7afdb308e3eee4b12c174e2e9402f55c98
RoyZhang8 <royzhang991220@gmail.com>
2022-02-08 01:19:45 -0600

    Final Update

--- ApplePicker-Unity/Assets/Scripts/ApplePicker.cs
+++ ApplePicker-Unity/Assets/Scripts/ApplePicker.cs
@@ -8,16 +8,19 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>();
         for (int i = 0; i < numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate(basketPrefab) as GameObject;
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
     }
 
@@ -29,9 +32,26 @@ public class ApplePicker : MonoBehaviour
     public void AppleDestroyed()
     {
         GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
-        foreach(GameObject tGo in tAppleArray)
+        foreach (GameObject tGo in tAppleArray)
         {
             Destroy(tGo);
         }
+
+        //// Destroy one of the Baskets
+        // Get the index of the last Basket in basketList
+        int basketIndex = basketList.Count - 1;
+        // Get a reference to that Basket GameObject
+        GameObject tBasketGO = basketList[basketIndex];
+        // Remove the Basket from the List and destroy the GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+
+        // Restart the game, which doesn't affect HighScore.Score
+        if (basketList.Count == 0)
+        {
+            Application.LoadLevel("_Scene_1");
+        }
+
+
     }
 }
---
+++ ApplePicker-Unity/Assets/Scripts/HighScore.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+    static public int score = 1000;
+
+    void Awake()
+    { // 1
+      // If the ApplePickerHighScore already exists, read it
+        if (PlayerPrefs.HasKey("HighScore"))
+        { // 2
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        // Assign the high score to ApplePickerHighScore
+        PlayerPrefs.SetInt("HighScore", score); // 3
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
+
+        // Update ApplePickerHighScore in PlayerPrefs if necessary
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        { // 4
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+
+    }
+}
